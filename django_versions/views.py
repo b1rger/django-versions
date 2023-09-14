fa6c13d30478f680c3387349997db3b34050a6fe
Birger Schacht <birger@rantanplan.org>
2023-09-14 13:39:23 +0200

    feat: make versions only accessible for logged in users

--- django_versions/views.py
+++ django_versions/views.py
@@ -1,11 +1,12 @@
 from django.views import View
 from django.http import JsonResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 import pkgutil
 import importlib
 
 
-class Versions(View):
+class Versions(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         versions = {}
         for _, name, ispkg in pkgutil.iter_modules():
